2b70ebb476261ccaee1dbb7622f1b7a7f149cdd2
AtwoodDeng <dengjdtsinghua@gmail.com>
2016-09-12 19:45:47 -0700

    init

---
+++ Assets/Script/Core/LogicManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LogicManager : MonoBehaviour {
+
+	public LogicManager() { s_Instance = this; }
+	public static LogicManager Instance { get { return s_Instance; } }
+	private static LogicManager s_Instance;
+		
+}
---
+++ Assets/Script/Core/M_Event.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class M_Event : MonoBehaviour {
+
+	/// <summary>
+	/// Event handler. handle the event with basic arg
+	/// </summary>
+	public delegate void EventHandler(BasicArg arg);
+
+	public static event EventHandler StartApp;
+	public static void FireStartApp(BasicArg arg){if ( StartApp != null ) StartApp(arg) ; }
+
+}
+
+public class BasicArg : EventArgs
+{
+	public BasicArg(object _this){m_sender = _this;}
+	object m_sender;
+	public object sender{get{return m_sender;}}
+}
+
+public class MsgArg : BasicArg
+{
+	public MsgArg(object _this):base(_this){}
+	Dictionary<string,object> m_dict;
+	Dictionary<string,object> dict
+	{
+		get {
+			if ( m_dict == null )
+				m_dict = new Dictionary<string, object>();
+			return m_dict;
+		}
+	}
+	public void AddMessage(string key, object val)
+	{
+		dict.Add(key, val);
+	}
+	public object GetMessage(string key)
+	{
+		object res;
+		dict.TryGetValue(key , out res);
+		return res;
+	}
+	public bool ContainMessage(string key)
+	{
+		return dict.ContainsKey(key);
+	}
+}
